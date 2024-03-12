7ff689288e3a605aeadbfe24767c8db4331624d4
douglasmcl <douglasmacielaraujo@outlook.com>
2024-03-12 17:58:32 -0300

    testing

--- jdbc2/src/application/Program.java
+++ jdbc2/src/application/Program.java
@@ -1,18 +1,43 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
-		System.out.println("ola");
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs  = st.executeQuery("SELECT * FROM Department");
+			
+			while(rs.next()) {
+				int id = rs.getInt("Id");
+				String name = rs.getString("Name");
+				System.out.println(id + ", " + name);
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
