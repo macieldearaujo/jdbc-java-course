7dc81723191049f5a1c257b097cd214c8cef35fc
douglasmcl <douglasmacielaraujo@outlook.com>
2024-03-13 00:22:49 -0300

    jdbc2 - insert data

--- jdbc2/src/application/Program.java
+++ jdbc2/src/application/Program.java
@@ -1,42 +1,61 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
-import db.DbException;
 
-public class Program {
+public class Program {		
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String args[]) {
+		
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement ps = null;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		try {
+			
+			// Example 1
 			conn = DB.getConnection();
+			ps = conn.prepareStatement(
+				"INSERT INTO seller" +
+				"(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+				"VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS
+			);
 			
-			st = conn.createStatement();
-			rs  = st.executeQuery("SELECT * FROM Department");
+			ps.setString(1, "Douglas Maciel");
+			ps.setString(2, "douglasmcl@gmail.com");
+			ps.setDate(3, new java.sql.Date(sdf.parse("27/08/2003").getTime()));
+			ps.setDouble(4, 2106.53);
+			ps.setInt(5, 2);
 			
-			while(rs.next()) {
-				int id = rs.getInt("Id");
-				String name = rs.getString("Name");
-				System.out.println(id + ", " + name);
+			int rowsAffected = ps.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id: " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
 			}
 		}
 		catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			e.printStackTrace();
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
 		}
 		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.closeStatement(ps);
+			DB.closeConnection(); // connection is always the last
 		}
 	}
-
 }
